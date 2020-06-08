9f503e8ceb62e61f79449a0fbcbb52810e072114
Nikita Kochnev <nikita.kochnev@justwebit.ru>
2020-06-08 19:05:31 +0300

    Deploy

--- test_exercise_leningrad_agency/src/App.js
+++ test_exercise_leningrad_agency/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Im a deployed
         </a>
       </header>
     </div>
